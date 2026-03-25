43d0694db101d9a3c587c742bbe95518a86653ab
Navroop3521 <navroopkaur2004@gmail.com>
2026-03-25 10:38:03 +0530

    Feet measurement equality

--- src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
+++ src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
@@ -3,11 +3,73 @@ package QuantityMeasurementApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MeasurementApplicationTests {
+//@SpringBootTest
 
-	@Test
-	void contextLoads() {
-	}
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class MeasurementApplicationTests {
+    @Test
+
+    void testFeetEquality_SameValue() {
+
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(89.6);
+
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(89.6);
+
+
+
+        assertEquals(f1, f2);
+
+    }
+
+
+
+    @Test
+
+    void testFeetEquality_DifferentValue() {
+
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(89.6);
+
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(34.6);
+
+
+
+        assertNotEquals(f1, f2);
+
+    }
+
+
+
+    @Test
+
+    void testFeetEquality_NullValue() {
+
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(5.0);
+
+
+
+        assertNotEquals(f1, null);
+
+    }
+
+
+
+    @Test
+
+    void testFeetEquality_DifferentType() {
+
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(5.0);
+
+        String other = "5.0";
+
+
+
+        assertNotEquals(f1, other);
+
+    }
 
 }
